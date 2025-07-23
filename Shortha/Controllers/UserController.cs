7c1e3360ffa76917929e37b94a2809e3b62fd410
Mahmoud Nasr <239.nasr@gmail.com>
2025-07-23 10:51:12 +0300

    Add GetUserById endpoint to UserController
    Introduces a new GET endpoint to retrieve user data by userId, with authorization to ensure users can only access their own data. Refactors user ID retrieval to use an extension method and improves code consistency.

--- Shortha/Controllers/UserController.cs
+++ Shortha/Controllers/UserController.cs
@@ -1,25 +1,37 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System.Security.Claims;
 using Shortha.Application.Services;
-using Shortha.Domain.Dto;
+using Shortha.Extenstions;
 
 namespace Shortha.Controllers
 {
     [Route("api/user")]
     [ApiController]
-    public class UserController(IUserService userService) :  Base
+    public class UserController(IUserService userService) : Base
     {
-      
         [Authorize]
         [HttpGet("sync")]
         public async Task<IActionResult> Sync()
         {
-            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var userId = User.GetUserId();
 
-            var user = await    userService.CreateUserAsync(userId);
-                
-                return Success(user);
+            var user = await userService.CreateUserAsync(userId);
+
+            return Success(user);
+        }
+
+        [Authorize]
+        [HttpGet("{userId}")]
+        public async Task<IActionResult> GetUserById(string userId)
+        {
+            var tokenUserId = User.GetUserId();
+            if (userId != tokenUserId)
+            {
+                return Fail("You can only access your own user data.");
+            }
+
+            var user = await userService.GetUserById(userId);
+            return Success(user);
         }
     }
 }
