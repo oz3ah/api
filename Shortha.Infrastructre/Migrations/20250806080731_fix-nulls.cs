bc4c48e589285c6c3a4d5de723d35b712e8730f9
Mahmoud Nasr <239.nasr@gmail.com>
2025-08-06 11:08:07 +0300

    Make Urls.CreatedBy and UpdatedBy nullable
    Updated the Urls table to allow null values for the CreatedBy and UpdatedBy columns. This change is reflected in a new migration and the model snapshot to support scenarios where these fields may not be set.

---
+++ Shortha.Infrastructre/Migrations/20250806080731_fix-nulls.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Shortha.Infrastructre.Migrations
+{
+    /// <inheritdoc />
+    public partial class fixnulls : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UpdatedBy",
+                table: "Urls",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CreatedBy",
+                table: "Urls",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UpdatedBy",
+                table: "Urls",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CreatedBy",
+                table: "Urls",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
