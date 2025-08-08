5b5477266a3ce6b39c9fdcffa7ca538c07532e8c
Mahmoud Nasr <239.nasr@gmail.com>
2025-08-08 17:55:21 +0300

    Refactor Serilog configuration for improved readability

--- Shortha.Infrastructre/DI/Serilog.cs
+++ Shortha.Infrastructre/DI/Serilog.cs
@@ -11,31 +11,31 @@ public static class SerilogConfiguration
     public static void AddSerilogLogging(this IHostBuilder builder)
     {
         builder.UseSerilog((context, config) =>
-{
-  config.Enrich.FromLogContext();
-  config.Enrich.WithExceptionDetails();
-  config.Enrich.With(new SerilogEnricher(
-    Config.appId,
-    Config.appName,
-    Config.env
-  ));
+        {
+            config.Enrich.FromLogContext();
+            config.Enrich.WithExceptionDetails();
+            config.Enrich.With(new SerilogEnricher(
+                                                   Config.appId,
+                                                   Config.appName,
+                                                   Config.env
+                                                  ));
 
-  config
-    .WriteTo.GrafanaLoki
-    (
-      "https://loki.gitnasr.com",
+            config
+                .WriteTo.GrafanaLoki
+                    (
+                     "https://loki.gitnasr.com",
       
-      new List<LokiLabel> { new() { Key = "appId", Value = Config.appId }, new() { Key = "appName", Value =
-                                                                             Config.appName
-                                                                           }, new() { Key = "env", Value = Config.env } },
-      period: TimeSpan.Zero
-    )
-    .WriteTo.Console
-    (
-      outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} level=[{Level:u3}] appId={ApplicationId} appName={ApplicationName} env={Environment} {Message:lj} {NewLine}{Exception}"
-    );
-  config.ReadFrom.Configuration(context.Configuration);
-});
+                     new List<LokiLabel> { new() { Key = "appId", Value = Config.appId }, new() { Key = "appName", Value =
+                                             Config.appName
+                                         }, new() { Key = "env", Value = Config.env } },
+                     period: TimeSpan.Zero
+                    )
+                .WriteTo.Console
+                    (
+                     outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} level=[{Level:u3}] appId={ApplicationId} appName={ApplicationName} env={Environment} {Message:lj} {NewLine}{Exception}"
+                    );
+            config.ReadFrom.Configuration(context.Configuration);
+        });
 
 
     }
