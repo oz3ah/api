c45a95403d83bb7675893836a54923ff80ce3591
Mahmoud Nasr <239.nasr@gmail.com>
2025-08-13 15:00:10 +0300

    Add permission check to visit analytics endpoint
    Introduced a check for 'view:analytics' permission in GetVisitsByShortUrl. Users without this permission now receive an error response when attempting to view visit analytics.

--- Shortha/Controllers/VisitController.cs
+++ Shortha/Controllers/VisitController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Shortha.Application.Services;
+using Shortha.Extenstions;
 
 namespace Shortha.Controllers
 {
@@ -10,8 +11,14 @@ namespace Shortha.Controllers
     {
         [HttpGet("{shortUrl}")]
         [Authorize]
-        public async Task<IActionResult> GetVisitsByShortUrl(string shortUrl, [FromQuery] int page = 1, [FromQuery] int pageSize = 10)
+        public async Task<IActionResult> GetVisitsByShortUrl(string shortUrl, [FromQuery] int page = 1,
+                                                             [FromQuery] int pageSize = 10)
         {
+            var userPermissions = User.GetPermissions();
+            if (!userPermissions.Contains("view:analytics"))
+            {
+                return Fail("You do not have permission to view visits.");
+            }
 
             var visits = await service.GetVisitsByShortUrl(shortUrl, page, pageSize);
 
