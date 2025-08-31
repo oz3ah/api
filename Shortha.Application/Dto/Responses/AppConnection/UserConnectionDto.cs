93203f31c4cf4522e4917e67d6117c03d318d21a
Mahmoud Nasr <239.nasr@gmail.com>
2025-08-31 23:22:38 +0300

    Add PairCode property to UserConnectionDto
    Updated the UserConnectionDto class in the Shortha.Application.Dto.Responses.AppConnection namespace to include a new required property, PairCode, of type string. This property will store a pairing code associated with the user connection.

--- Shortha.Application/Dto/Responses/AppConnection/UserConnectionDto.cs
+++ Shortha.Application/Dto/Responses/AppConnection/UserConnectionDto.cs
@@ -7,6 +7,7 @@ public class UserConnectionDto
     public required string Id { get; set; }
     public string? Status { get; set; }
     public string? Device { get; set; }
+    public required string PairCode { get; set; }
     public DateTime? UpdatedAt { get; set; }
     public DateTime? ActivatedAt { get; set; }
     public string Version { get; set; }
