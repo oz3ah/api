97a625fc6e5317a35730cee8ebc730c52dd8866c
Mahmoud Nasr <239.nasr@gmail.com>
2025-09-04 00:13:32 +0300

    Add ActivityController for API endpoint handling
    Introduces a new `ActivityController` class in the `Shortha.Controllers` namespace. The controller is set up to handle HTTP requests at the "api/activity" endpoint, utilizing the `[Route]` and `[ApiController]` attributes. Includes necessary `using` directive for `Microsoft.AspNetCore.Mvc` to support API functionality.

---
+++ Shortha/Controllers/ActivityController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shortha.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ActivityController : ControllerBase
+    {
+    }
+}
